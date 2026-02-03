9565bfdcaabc780dc61b245b76e74b8a86b4dad5
Marios Kogias <marioskogias@gmail.com>
2026-02-03 22:38:14 +0000

    Configure kubeshark

--- scripts/install-kubeshark.sh
+++ scripts/install-kubeshark.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 helm install kubeshark kubeshark/kubeshark \
   -n kubeshark --create-namespace \
-  --version 52.4
+  --version 52.4 -f configs/kubeshark-values.yaml
 
 # To uninstall run:
 # helm uninstall kubeshark -n kubeshark
